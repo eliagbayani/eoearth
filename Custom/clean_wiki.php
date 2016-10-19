b3567c767f1d5a428fab2439f147810a675b1252
eliagbayani <eli@eol.org>
2016-10-19 06:41:08 -0400

    updates

--- Custom/clean_wiki.php
+++ Custom/clean_wiki.php
@@ -158,7 +158,7 @@ function process_urls($ver)
             // $urls = array("/eoearth/wiki/Climate_Change_(search_results_for)");
             // $urls = array("/eoearth/wiki/Ecology_(search_results_for)");
 
-            // /*
+            // /* current - done
             $urls = array();
             $urls[] = "/eoearth/wiki/About_the_EoE_(search_results_for)";
             $urls[] = "/eoearth/wiki/Biodiversity_(search_results_for)";
@@ -166,6 +166,30 @@ function process_urls($ver)
             $urls[] = "/eoearth/wiki/Climate_Change_(search_results_for)";
             $urls[] = "/eoearth/wiki/Ecology_(search_results_for)";
             // */
+            
+
+            // /* next
+            $urls = array();
+            $urls[] = "/eoearth/wiki/Environmental_%26_Earth_Science_(search_results_for)";
+            $urls[] = "/eoearth/wiki/Energy_(search_results_for)";
+            $urls[] = "/eoearth/wiki/Environmental_Law_%26_Policy_(search_results_for)";
+            $urls[] = "/eoearth/wiki/Environmental_Humanities_(search_results_for)";
+            $urls[] = "/eoearth/wiki/Food_(search_results_for)";
+            $urls[] = "/eoearth/wiki/Forests_(search_results_for)";
+            $urls[] = "/eoearth/wiki/Geography_(search_results_for)";
+            $urls[] = "/eoearth/wiki/Hazards_%26_Disasters_(search_results_for)";
+            $urls[] = "/eoearth/wiki/Health_(search_results_for)";
+            $urls[] = "/eoearth/wiki/Mining_%26_Materials_(search_results_for)";
+            $urls[] = "/eoearth/wiki/People_(search_results_for)";
+            $urls[] = "/eoearth/wiki/Physics_%26_Chemistry_(search_results_for)";
+            $urls[] = "/eoearth/wiki/Pollution_(search_results_for)";
+            $urls[] = "/eoearth/wiki/Society_%26_Environment_(search_results_for)";
+            $urls[] = "/eoearth/wiki/Water_(search_results_for)";
+            $urls[] = "/eoearth/wiki/Weather_%26_Climate_(search_results_for)";
+            $urls[] = "/eoearth/wiki/Wildlife_(search_results_for)";
+            
+            
+            
 
             // $urls = array("/eoearth/wiki/Environmental_%26_Earth_Science_(search_results_for)");
             // $urls = array("/eoearth/wiki/Energy_(search_results_for)");
