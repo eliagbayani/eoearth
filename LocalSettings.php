da283ca299d49de9c04b14c02171617d95cddb7a
eliagbayani <eli@eol.org>
2017-01-17 00:33:25 -0500

    add google analytics extension

--- LocalSettings.php
+++ LocalSettings.php
@@ -593,3 +593,26 @@ html = html.replace("Last", "Wiki last");
 */
 // require_once "$IP/extensions/LastModified/LastModified.php";
 require_once( "$IP/extensions/LastModified/LastModified.php" );
+
+// for google analytics extension
+require_once "$IP/extensions/googleAnalytics/googleAnalytics.php";
+// Replace xxxxxxx-x with YOUR GoogleAnalytics UA number
+// $wgGoogleAnalyticsAccount = 'UA-xxxxxxx-x'; 
+$wgGoogleAnalyticsAccount = 'UA-3298646-15';                              
+// Add HTML code for any additional web analytics (can be used alone or with $wgGoogleAnalyticsAccount)
+$wgGoogleAnalyticsOtherCode = '<script type="text/javascript" src="https://analytics.example.com/tracking.js"></script>';
+
+// Optional configuration (for defaults see googleAnalytics.php)
+// Store full IP address in Google Universal Analytics (see https://support.google.com/analytics/answer/2763052?hl=en for details)
+$wgGoogleAnalyticsAnonymizeIP = false; 
+// Array with NUMERIC namespace IDs where web analytics code should NOT be included.
+// $wgGoogleAnalyticsIgnoreNsIDs = array(500);
+// Array with page names (see magic word Extension:Google Analytics Integration) where web analytics code should NOT be included.
+// $wgGoogleAnalyticsIgnorePages = array('ArticleX', 'Foo:Bar');
+// Array with special pages where web analytics code should NOT be included.
+// $wgGoogleAnalyticsIgnoreSpecials = array( 'Userlogin', 'Userlogout', 'Preferences', 'ChangePassword', 'OATH');
+// Use 'noanalytics' permission to exclude specific user groups from web analytics, e.g.
+// $wgGroupPermissions['sysop']['noanalytics'] = true;
+// $wgGroupPermissions['bot']['noanalytics'] = true;
+// To exclude all logged in users give 'noanalytics' permission to 'user' group, i.e.
+// $wgGroupPermissions['user']['noanalytics'] = true;
