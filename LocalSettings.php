b0c010c4b26cf91e84a5f25a1bbd29df362bcccb
eliagbayani <eli@eol.org>
2017-02-02 11:41:59 -0500

    update

--- LocalSettings.php
+++ LocalSettings.php
@@ -295,10 +295,10 @@ $wgVisualEditorNamespaces[] = NS_TEMPLATE;
 $wgVisualEditorNamespaces[] = NS_TEMPLATE_TALK;
 */
 
-/*
+// /*
 $wgContentNamespaces[] = NS_TEMPLATE; //this adds the Template namespace in VisualEditor
 $wgContentNamespaces[] = NS_TEMPLATE_TALK; //this adds this namespace in VisualEditor
-*/
+// */
 
 ///* not needed on mac mini
 $wgVirtualRestConfig['modules']['parsoid'] = array(
