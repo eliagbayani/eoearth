fd174f106788a584755322b77a69845f97e72ecb
eliagbayani <eli@eol.org>
2017-02-02 03:24:29 -0500

    new TemplateData extension

---
+++ extensions/TemplateData/Gruntfile.js
@@ -0,0 +1,56 @@
+/*!
+ * Grunt file
+ *
+ * @package TemplateData
+ */
+
+/*jshint node:true */
+module.exports = function ( grunt ) {
+	var conf = grunt.file.readJSON( 'extension.json' );
+
+	grunt.loadNpmTasks( 'grunt-banana-checker' );
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-contrib-watch' );
+	grunt.loadNpmTasks( 'grunt-jscs' );
+	grunt.loadNpmTasks( 'grunt-jsonlint' );
+	grunt.loadNpmTasks( 'grunt-stylelint' );
+
+	grunt.initConfig( {
+		jshint: {
+			options: {
+				jshintrc: true
+			},
+			all: [
+				'modules/**/*.js',
+				'tests/**/*.js'
+			]
+		},
+		jscs: {
+			src: '<%= jshint.all %>'
+		},
+		stylelint: {
+			all: [
+				'modules/*.css',
+				'resources/*.css'
+			]
+		},
+		jsonlint: {
+			all: [
+				'**/*.json',
+				'!node_modules/**'
+			]
+		},
+		banana: conf.MessagesDirs,
+		watch: {
+			files: [
+				'.{stylelintrc,jscsrc,jshintignore,jshintrc}',
+				'<%= jshint.all %>',
+				'<%= stylelint.all %>'
+			],
+			tasks: 'test'
+		}
+	} );
+
+	grunt.registerTask( 'test', [ 'jshint', 'jscs', 'stylelint', 'jsonlint', 'banana' ] );
+	grunt.registerTask( 'default', 'test' );
+};
